5915cdb1c7e3bdb692423df98679c29d39c5d6d3
Mehmet Öztürk <mozturk357@gmail.com>
2020-11-09 13:18:33 +0300

    ekle

---
+++ Entities/Tables/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Tables
+{
+    [Table("Admin",Schema ="MusteriSistemi")]
+    public class Admin
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [Required]
+        [MaxLength(32)]
+        [MinLength(5)]
+        public string Username { get; set; }
+        [Required]
+        [MaxLength(32)]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
---
+++ Entities/Tables/Musteri.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Entities.Tables
+{
+    [Table("TBL_Musteri",Schema="MusteriSistemi")]
+    public class Musteri
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        [DisplayName("as")]
+        [Required]
+        [MaxLength(150)]
+        [MinLength(5)]
+        public string NameSurname { get; set; }
+        [Required]
+        public String Email { get; set; }
+
+    }
+}
